b4451f81046648f83b983c1d376b379ec499faf7
wolf-dominion <hannah_drury@yahoo.com>
2020-04-07 10:37:25 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
-  
-end
+    attr_accessor :name, :age
+   
+    @@all = []
+   
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
+   
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    # returns all the waiter instances that are associated with the current customer instance 
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+      end
+
+  end
